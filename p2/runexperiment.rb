a263c5ba77071d7d361918ad485951c24d142920
David Charte <fdavidcl@protonmail.com>
2017-03-14 19:39:12 +0100

    Adjustments in script

--- p2/runexperiment.rb
+++ p2/runexperiment.rb
@@ -4,16 +4,11 @@ header = <<EOF
 num_procs, num_ints, err_excess, err_defect, err_middle, t_calc, t_comms, t_create, t_destroy
 EOF
 
-lim_procs = 4
-lim_exp   = 8
-
 if ARGV[0] == "atcgrid"
-  lim_procs = 3
-  lim_exp   = 8
   `scp pi-mpi acap4@atcgrid.ugr.es:/home/acap4`
 end
 
-outlines = [2, 3].map do |proc_num|
+outlines = [1, 2, 3, 6].map do |proc_num|
   (1 .. 9).map do |exp|
     if ARGV[0] == "atcgrid"
       cmd = %(ssh acap4@atcgrid.ugr.es 'echo "/usr/lib64/openmpi/bin/mpiexec -n #{proc_num} pi-mpi #{10 ** exp}" | qsub -q acap')
